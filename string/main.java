ac38c5b8b33778ff94cb2faf1117c61694846f6f
keshav15thakur <keshavthakur101299@gmail.com>
2024-04-27 15:32:12 +0530

    java string program

---
+++ string/main.java
@@ -0,0 +1,16 @@
+// Write a Java program to get the character at the given index within the string.
+
+// Sample Output:
+
+// Original String = Java Exercises!                                                                             
+// The character at position 0 is J                                                                              
+// The character at position 10 is i 
+
+
+public class main{
+    public static void main(String[] args) {
+       //System.out.println("keshav \"thakur\"");
+       System.out.println("keshav 'thakur'");
+       
+}
+}
---
+++ string/question1.java
@@ -0,0 +1,30 @@
+// Write a Java program to get the character at the given index within the string.
+
+// Sample Output:
+
+// Original String = Java Exercises!                                                                             
+// The character at position 0 is J                                                                              
+// The character at position 10 is i 
+
+
+
+
+
+// public class question1 {
+//     public static void main(String[] args) {
+//        String val = "Java Exercises!";
+//        System.out.println("original string  = " + val) ;
+//     }  
+// }
+
+
+
+
+
+public class question1 {
+    public static void main(String[] args) {
+       String val = "Java Exercises!";
+       System.out.println(val.indexOf(j)) ;
+    }  
+}
+
